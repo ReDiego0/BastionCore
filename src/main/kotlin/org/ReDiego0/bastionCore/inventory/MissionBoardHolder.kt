bf07c8c8727eac0cba4e256c3086915895b3cb9e
ReDiego0 <re.diego.maldonado@gmail.com>
2026-02-01 22:32:57 -0300

    feat: Add MissionBoardHolder as a custom InventoryHolder for mission board functionality

---
+++ src/main/kotlin/org/ReDiego0/bastionCore/inventory/MissionBoardHolder.kt
@@ -0,0 +1,10 @@
+package org.ReDiego0.bastionCore.inventory
+
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+
+class MissionBoardHolder : InventoryHolder {
+    override fun getInventory(): Inventory {
+        return null!!
+    }
+}
