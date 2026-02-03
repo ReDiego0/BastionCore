0a729e55612d4cd93692475ed1a4ca9406d9b217
ReDiego0 <re.diego.maldonado@gmail.com>
2026-02-03 02:55:01 -0300

    feat: Reorganize and optimize combat event handlers in `CombatListener`
    - Refactored method ordering for clearer prioritization of combat events.
    - Improved `onProjectileHit` logic with enhanced checks, effects, and entity handling.
    - Adjusted slowness effect duration and projectile velocity calculations for better gameplay balance.

--- src/main/kotlin/org/ReDiego0/bastionCore/listener/CombatListener.kt
+++ src/main/kotlin/org/ReDiego0/bastionCore/listener/CombatListener.kt
@@ -20,6 +20,62 @@ import org.bukkit.potion.PotionEffectType
 
 class CombatListener(private val plugin: BastionCore) : Listener {
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onDefend(event: EntityDamageByEntityEvent) {
+        val victim = event.entity
+        if (victim !is Player) return
+        if (!victim.world.name.startsWith("inst_")) return
+
+        if (plugin.combatManager.isBlocking(victim.uniqueId)) {
+            event.isCancelled = true
+            event.damage = 0.0
+            plugin.combatManager.spearHandler.triggerExplosiveCounter(victim)
+            return
+        }
+
+        if (plugin.combatManager.isParrying(victim.uniqueId)) {
+            event.isCancelled = true
+            event.damage = 0.0
+            plugin.combatManager.katanaHandler.triggerParryCounter(victim)
+            return
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onAttack(event: EntityDamageByEntityEvent) {
+        if (event.isCancelled) return
+        if (event.damager !is Player) return
+        if (event.entity !is LivingEntity) return
+
+        val player = event.damager as Player
+
+        if (!player.world.name.startsWith("inst_")) return
+
+        val data = plugin.playerDataManager.getData(player.uniqueId) ?: return
+        val weaponType = ItemTags.getWeaponType(player.inventory.itemInMainHand)
+
+        val gain = if (weaponType != WeaponType.NONE) 2.0 else 0.5
+
+        if (data.ultimateCharge < 100.0) {
+            data.addCharge(player, gain)
+            updateMissionBar(player, data.ultimateCharge)
+        }
+    }
+
+    @EventHandler
+    fun onKill(event: EntityDeathEvent) {
+        val killer = event.entity.killer ?: return
+        if (!killer.world.name.startsWith("inst_")) return
+
+        val data = plugin.playerDataManager.getData(killer.uniqueId) ?: return
+        val gainOnKill = plugin.config.getDouble("combat.kill_ultimate_gain", 10.0)
+
+        if (data.ultimateCharge < 100.0) {
+            data.addCharge(killer, gainOnKill)
+            updateMissionBar(killer, data.ultimateCharge)
+        }
+    }
+
     @EventHandler
     fun onProjectileHit(event: ProjectileHitEvent) {
         val projectile = event.entity
@@ -28,6 +84,7 @@ class CombatListener(private val plugin: BastionCore) : Listener {
 
         val loc = projectile.location
         val world = projectile.world
+        val shooter = projectile.shooter as? Player
 
         world.spawnParticle(Particle.EXPLOSION, loc, 1)
         world.playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, 1f, 0.8f)
@@ -35,72 +92,18 @@ class CombatListener(private val plugin: BastionCore) : Listener {
         projectile.remove()
 
         val radius = 4.0
-        val shooter = projectile.shooter as? Player
 
         for (entity in world.getNearbyEntities(loc, radius, radius, radius)) {
             if (entity is LivingEntity && entity != shooter) {
                 entity.damage(15.0, shooter)
-                entity.addPotionEffect(PotionEffect(PotionEffectType.SLOWNESS, 5, 2))
+
+                entity.addPotionEffect(PotionEffect(PotionEffectType.SLOWNESS, 60, 2))
                 val dir = entity.location.toVector().subtract(loc.toVector()).normalize()
+                if (dir.lengthSquared() < 0.01) dir.setY(1.0)
+
                 entity.velocity = dir.multiply(1.5).setY(0.5)
             }
         }
-
-        @EventHandler(priority = EventPriority.LOWEST)
-        fun onDefend(event: EntityDamageByEntityEvent) {
-            val victim = event.entity
-            if (victim !is Player) return
-            if (!victim.world.name.startsWith("inst_")) return
-
-            if (plugin.combatManager.isBlocking(victim.uniqueId)) {
-                event.isCancelled = true
-                event.damage = 0.0
-                plugin.combatManager.spearHandler.triggerExplosiveCounter(victim)
-                return
-            }
-
-            if (plugin.combatManager.isParrying(victim.uniqueId)) {
-                event.isCancelled = true
-                event.damage = 0.0
-                plugin.combatManager.katanaHandler.triggerParryCounter(victim)
-                return
-            }
-        }
-
-        @EventHandler(priority = EventPriority.HIGH)
-        fun onAttack(event: EntityDamageByEntityEvent) {
-            if (event.isCancelled) return
-            if (event.damager !is Player) return
-            if (event.entity !is LivingEntity) return
-
-            val player = event.damager as Player
-            if (!player.world.name.startsWith("inst_")) return
-
-            val data = plugin.playerDataManager.getData(player.uniqueId) ?: return
-            val weaponType = ItemTags.getWeaponType(player.inventory.itemInMainHand)
-
-            val gain = if (weaponType != WeaponType.NONE) 2.0 else 0.5
-
-            if (data.ultimateCharge < 100.0) {
-                data.addCharge(player, gain)
-                updateMissionBar(player, data.ultimateCharge)
-            }
-        }
-
-        @EventHandler
-        fun onKill(event: EntityDeathEvent) {
-            val killer = event.entity.killer ?: return
-            if (!killer.world.name.startsWith("inst_")) return
-
-            val data = plugin.playerDataManager.getData(killer.uniqueId) ?: return
-            val gainOnKill = plugin.config.getDouble("combat.kill_ultimate_gain", 10.0)
-
-            if (data.ultimateCharge < 100.0) {
-                data.addCharge(killer, gainOnKill)
-                updateMissionBar(killer, data.ultimateCharge)
-            }
-        }
-
     }
 
     private fun updateMissionBar(player: Player, charge: Double) {
